b088dcd0a026e4c0da4bfed75d0ae0235db5ebab
syh <632541058@qq.com>
2015-04-26 11:06:14 +0800

    update

--- app/src/main/java/com/neu/strangers/activities/MainActivity.java
+++ app/src/main/java/com/neu/strangers/activities/MainActivity.java
@@ -76,8 +76,21 @@ public class MainActivity extends AppCompatActivity{
 		mSensorManager.registerListener(mListener, mSensor, SensorManager.SENSOR_DELAY_NORMAL);
 	}
 
+    @Override
+    protected void onStop() {
+        if(mSensorManager != null)
+            mSensorManager.unregisterListener(mListener);
+        super.onStop();
+    }
 
-	@Override
+    @Override
+    protected void onRestart() {
+        mSensorManager.registerListener(mListener,mSensor,SensorManager.SENSOR_DELAY_NORMAL);
+        super.onRestart();
+    }
+
+
+    @Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.menu_main, menu);
