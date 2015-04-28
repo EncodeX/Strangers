a8ab5582a3f291522c5a004b559c317714ce30b8
syh <632541058@qq.com>
2015-04-28 21:54:06 +0800

    add the function to locate

--- app/src/main/java/com/neu/strangers/activities/NearbyStrangers.java
+++ app/src/main/java/com/neu/strangers/activities/NearbyStrangers.java
@@ -5,10 +5,17 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.View;
 import android.widget.RelativeLayout;
-import android.widget.Toast;
-
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
 import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
 import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.neu.strangers.R;
 import com.readystatesoftware.systembartint.SystemBarTintManager;
 
@@ -20,6 +27,38 @@ public class NearbyStrangers extends Activity{
     private SystemBarTintManager mSystemBarTintManager;
     private MapView mMapView;
     private RelativeLayout mProgressLayout;
+    private BaiduMap mBaiduMap;
+    private LocationClient mLocClient;
+    private MyLocationListenner mListener = new MyLocationListenner();
+    boolean mIsFirstLoc = true;   // if is the first time to locate
+
+
+    private class MyLocationListenner implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+
+
+            if (location == null || mMapView == null)
+                return;
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (mIsFirstLoc) {
+                mIsFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatusUpdate u = MapStatusUpdateFactory.newLatLng(ll);
+                mBaiduMap.animateMapStatus(u);
+            }
+
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,18 +72,36 @@ public class NearbyStrangers extends Activity{
         mMapView = (MapView)findViewById(R.id.bmapView);
         mProgressLayout = (RelativeLayout)findViewById(R.id.progress_layout);
 
+        // Initialize MapView
+        mMapView = (MapView) findViewById(R.id.bmapView);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationEnabled(true);
+        // Initialize LocationClint 
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(mListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true);
+        option.setCoorType("bd09ll");
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        // start locating
+        mLocClient.start();
+
 
         //Just for fun :)
         new Handler().postDelayed(new Runnable() {
             public void run() {
-
                 mMapView.setVisibility(View.VISIBLE);
                 mProgressLayout.setVisibility(View.GONE);
-
 
             }
         }, 3000);
 
+
     //    Toast.makeText(NearbyStrangers.this,"Test Test Test",Toast.LENGTH_SHORT).show();
     }
+
+
+
+
 }
