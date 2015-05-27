28bb2e1d4b54e43a126c078e9f1db81264f4ebea
Encode.X <encode.xii@gmail.com>
2015-05-28 00:20:08 +0800

    初步实现联系人列表

--- app/src/main/java/com/neu/strangers/view/MainViewPager.java
+++ app/src/main/java/com/neu/strangers/view/MainViewPager.java
@@ -9,24 +9,17 @@ import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.RelativeLayout;
-import android.widget.SectionIndexer;
 import android.widget.TextView;
 
 import com.astuetz.PagerSlidingTabStrip;
-import com.balysv.materialripple.MaterialRippleLayout;
 import com.neu.strangers.R;
+import com.neu.strangers.adapter.ContactAdapter;
 import com.neu.strangers.adapter.SimpleItemAdapter;
-import com.woozzu.android.util.StringMatcher;
 import com.woozzu.android.widget.IndexableListView;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-
-import butterknife.ButterKnife;
-import butterknife.InjectView;
 
 /**
  * Created with Android Studio.
@@ -39,6 +32,7 @@ public class MainViewPager extends ViewPager {
 	private List<View> mViewList;
 	RecyclerView mRecentChatList;
 	IndexableListView mContactsList;
+	ContactAdapter mAdapter;
 
 	public MainViewPager(Context context, AttributeSet attrs) {
 		super(context, attrs);
@@ -62,31 +56,41 @@ public class MainViewPager extends ViewPager {
 		// Initialize contacts list
 		mContactsList = (IndexableListView)contactsView.findViewById(R.id.contacts_list);
 		ArrayList<String> mItems= new ArrayList<String>();
-		mItems.add("Diary of a Wimpy Kid 6: Cabin Fever");
-		mItems.add("Steve Jobs");
-		mItems.add("Inheritance (The Inheritance Cycle)");
-		mItems.add("11/22/63: A Novel");
-		mItems.add("The Hunger Games");
-		mItems.add("The LEGO Ideas Book");
-		mItems.add("Explosive Eighteen: A Stephanie Plum Novel");
-		mItems.add("Catching Fire (The Second Book of the Hunger Games)");
-		mItems.add("Elder Scrolls V: Skyrim: Prima Official Game Guide");
-		mItems.add("Death Comes to Pemberley");
-		mItems.add("Diary of a Wimpy Kid 6: Cabin Fever");
-		mItems.add("Steve Jobs");
-		mItems.add("Inheritance (The Inheritance Cycle)");
-		mItems.add("11/22/63: A Novel");
-		mItems.add("The Hunger Games");
-		mItems.add("The LEGO Ideas Book");
-		mItems.add("Explosive Eighteen: A Stephanie Plum Novel");
-		mItems.add("Catching Fire (The Second Book of the Hunger Games)");
-		mItems.add("Elder Scrolls V: Skyrim: Prima Official Game Guide");
-		mItems.add("Death Comes to Pemberley");
+		mItems.add("aback");
+		mItems.add("abash");
+		mItems.add("abbey");
+		mItems.add("abhor");
+		mItems.add("abide");
+		mItems.add("abuse");
+		mItems.add("candidate");
+		mItems.add("capture");
+		mItems.add("careful");
+		mItems.add("catch");
+		mItems.add("cause");
+		mItems.add("celebrate");
+		mItems.add("forever");
+		mItems.add("fable");
+		mItems.add("fidelity");
+		mItems.add("fox");
+		mItems.add("funny");
+		mItems.add("fail");
+		mItems.add("jail");
+		mItems.add("jade");
+		mItems.add("jailor");
+		mItems.add("january");
+		mItems.add("jasmine");
+		mItems.add("jazz");
+		mItems.add("zero");
+		mItems.add("zoo");
+		mItems.add("zeus");
+		mItems.add("zebra");
+		mItems.add("zest");
+		mItems.add("zing");
 		Collections.sort(mItems);
 
-		ContentAdapter adapter = new ContentAdapter(context, android.R.layout.simple_list_item_1, mItems);
+		mAdapter = new ContactAdapter(mItems,context);
 
-		mContactsList.setAdapter(adapter);
+		mContactsList.setAdapter(mAdapter);
 		mContactsList.setFastScrollEnabled(true);
 
 		mViewList.add(recentChatView);
@@ -132,49 +136,6 @@ public class MainViewPager extends ViewPager {
 		@Override
 		public void destroyItem(ViewGroup container, int position, Object object) {
 			container.removeView(mViewList.get(position));
-		}
-	}
-
-	private class ContentAdapter extends ArrayAdapter<String> implements SectionIndexer {
-
-		private String mSections = "#ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-
-		public ContentAdapter(Context context, int textViewResourceId,
-		                      List<String> objects) {
-			super(context, textViewResourceId, objects);
-		}
-
-		@Override
-		public int getPositionForSection(int section) {
-			// If there is no item for current section, previous section will be selected
-			for (int i = section; i >= 0; i--) {
-				for (int j = 0; j < getCount(); j++) {
-					if (i == 0) {
-						// For numeric section
-						for (int k = 0; k <= 9; k++) {
-							if (StringMatcher.match(String.valueOf(getItem(j).charAt(0)), String.valueOf(k)))
-								return j;
-						}
-					} else {
-						if (StringMatcher.match(String.valueOf(getItem(j).charAt(0)), String.valueOf(mSections.charAt(i))))
-							return j;
-					}
-				}
-			}
-			return 0;
-		}
-
-		@Override
-		public int getSectionForPosition(int position) {
-			return 0;
-		}
-
-		@Override
-		public Object[] getSections() {
-			String[] sections = new String[mSections.length()];
-			for (int i = 0; i < mSections.length(); i++)
-				sections[i] = String.valueOf(mSections.charAt(i));
-			return sections;
 		}
 	}
 }
