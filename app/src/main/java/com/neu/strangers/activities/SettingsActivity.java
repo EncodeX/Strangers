2a9df98758389c096c854a3959b6f05be4e54427
Encode.X <encode.xii@gmail.com>
2015-05-27 11:45:29 +0800

    Modified Settings Sample Activity

--- app/src/main/java/com/neu/strangers/activities/SettingsActivity.java
+++ app/src/main/java/com/neu/strangers/activities/SettingsActivity.java
@@ -9,6 +9,7 @@ import com.kenumir.materialsettings.MaterialSettingsActivity;
 import com.kenumir.materialsettings.items.CheckboxItem;
 import com.kenumir.materialsettings.items.HeaderItem;
 import com.kenumir.materialsettings.items.SwitcherItem;
+import com.kenumir.materialsettings.items.TextItem;
 import com.kenumir.materialsettings.storage.PreferencesStorageInterface;
 import com.kenumir.materialsettings.storage.StorageInterface;
 import com.neu.strangers.R;
@@ -48,23 +49,23 @@ public class SettingsActivity extends MaterialSettingsActivity {
 		mSystemBarTintManager.setTintColor(getResources().getColor(R.color.app_color_primary_dark));
 
 		addItem(new HeaderItem(getFragment()).setTitle("Sample title 1"));
-		addItem(new CheckboxItem(getFragment(), "key1").setTitle("Checkbox item 1").setSubtitle("Subtitle text 1").setOnCheckedChangeListener(new CheckboxItem.OnCheckedChangeListener() {
+		addItem(new CheckboxItem(getFragment(), "key1").setTitle("Checkbox item").setSubtitle("Subtitle text").setOnCheckedChangeListener(new CheckboxItem.OnCheckedChangeListener() {
 			@Override
 			public void onCheckedChange(CheckboxItem cbi, boolean isChecked) {
 				Toast.makeText(SettingsActivity.this, "CHECKED: " + isChecked, Toast.LENGTH_SHORT).show();
 			}
 		}));
-		addItem(new SwitcherItem(getFragment(), "key2").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key3").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key4").setTitle("Checkbox item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key2").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key3").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new TextItem(getFragment(), "key4").setTitle("Text item"));
 		addItem(new HeaderItem(getFragment()).setTitle("Sample title 2"));
-		addItem(new SwitcherItem(getFragment(), "key5").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key6").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key7").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key8").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key9").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key10").setTitle("Checkbox item").setSubtitle("Subtitle text"));
-		addItem(new SwitcherItem(getFragment(), "key11").setTitle("Checkbox item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key5").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key6").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key7").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key8").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key9").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key10").setTitle("Switcher item").setSubtitle("Subtitle text"));
+		addItem(new SwitcherItem(getFragment(), "key11").setTitle("Switcher item").setSubtitle("Subtitle text"));
 	}
 
 	@Override
